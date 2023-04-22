7bd44073d7130b4edd15c75c96f6c21aad34b7c1
elenaeslin <srta.scobar@gmail.com>
2023-04-22 15:46:14 +0200

    make function random choice

--- odin-rock-paper-scissors/script.js
+++ odin-rock-paper-scissors/script.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+
+// Get Computer choice as a random number
+function getComputerChoice() {
+    // Define three possible choices
+    let choice = ["Rock", "Paper", "Scissors"];
+
+    // Return and integer between 0 and 2 both inclusive randomly selected
+    // Math.random gets random float between 0 and 1 exclusive
+    // Making * 3 makes the coice between 0 and 3 exclusive [0.0 - 2.99]
+    // Math.floor makes that choices between 0, 1 or 2
+    return choice[Math.floor(Math.random() * 3)];
+}
+
+// Function of a round that takes two selections and return the winner
+// 1- If player = "Rock" and computer = "Paper" 
+function playRound(playerSelection, computerSelection) {
+
+}
+
+console.log(getComputerChoice());
+
