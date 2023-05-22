c3932d8fe61db9c6ad7862fb7abe9eb51d4657f6
elenaeslin <srta.scobar@gmail.com>
2023-05-22 17:33:10 +0200

    Add drag and drop function

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,49 +1,66 @@
-let GRID_SIZE = 16;
-const container = document.querySelector(".container");
-const btn = document.getElementById("btnSize");
+const DEFAULT_SIZE = 16;
+const DEFAULT_COLOR = '#000';
+const DEFAULT_MODE = 'black';
+
+let gridMode = DEFAULT_MODE;
+let gridColor = DEFAULT_COLOR;
+let gridSize = DEFAULT_SIZE;
+
+
+const grid = document.querySelector(".grid");
+const btnSize = document.getElementById("btnSize");
 const btnErase = document.getElementById("btnErase");
 const colorChoices = document.querySelectorAll(".colorChoice");
 const btnColor = document.getElementById("btnColor");
 
-let color = 'rgb(0,0,0)';
+let mouseDown = false;
+document.body.onmousedown = () => mouseDown = true;
+document.body.onmouseup = () => mouseDown = false;
 
-set_grid();
-draw();
+setGrid();
 
-btn.addEventListener('click', () => {
-    let newSize = prompt("Enter new size: ");
-    GRID_SIZE = newSize;
+// btn.addEventListener('click', () => {
+//     let newSize = prompt("Enter new size: ");
+//     GRID_SIZE = newSize;
 
-    set_grid();
-    draw();
-});
-btnErase.addEventListener('click', () => {
-    set_grid();
-    draw();
-});
-btnColor.addEventListener('click', () => {
+//     setGrid();
+//     draw();
+// });
+// btnErase.addEventListener('click', () => {
+//     setGrid();
+//     draw();
+// });
+// btnColor.addEventListener('click', () => {
 
-    colorChoices.forEach(choice => {
-        if (choice.checked) {
-            color = choice.value;
-        }
-    });
+//     colorChoices.forEach(choice => {
+//         if (choice.checked) {
+//             color = choice.value;
+//         }
+//     });
     
-});
+// });
+function clearGrid() {
+    grid.innerHTML = '';
+}
+function setGrid() {
+    grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 
-function set_grid() {
-    container.innerHTML='';
-    let w = 1000/GRID_SIZE;
-    for (let row = 0; row < GRID_SIZE; row++) {
-        for (let col = 0; col < GRID_SIZE; col++) {
+    for (let row = 0; row < gridSize; row++) {
+        for (let col = 0; col < gridSize; col++) {
             let div = document.createElement("div");
             div.classList.add("square");
-            div.style.width = `${w}px`;
-            div.style.height = `${w}px`;
-            container.appendChild(div);
+            div.addEventListener('mouseover', changeColor);
+            div.addEventListener('mousedown', changeColor);
+            grid.appendChild(div);
         }
     }
-    console.log(GRID_SIZE)
+}
+
+function changeColor(e) {
+    if (e.type == 'mouseover' && !mouseDown) return
+    if (colorMode == 'black') {
+        e.target.style.backgroundColor = 'blue';
+    }
 }
 
 function setRandomColor() {
@@ -55,21 +72,21 @@ function setRandomColor() {
 }
 
 function draw() {
-    const squares = document.querySelectorAll(".square");
-    let clickCounter = 0;
-    squares.forEach(square => square.addEventListener('mouseover', () => {
-        clickCounter++;
-        if (color == 'random') {
-            color = setRandomColor();
-        }
-        square.style.backgroundColor = color;
-        // squares.forEach(hoveredSquare => hoveredSquare.addEventListener('mouseover', () => {
-        //     if (clickCounter % 2 == 0) {
-        //         hoveredSquare.style.backgroundColor = 'white';
-        //     } else {
-        //         hoveredSquare.style.backgroundColor = 'black';
-        //     }
-        // }));
+    // const squares = document.querySelectorAll(".square");
+    // let clickCounter = 0;
+    // squares.forEach(square => square.addEventListener('click', () => {
+    //     clickCounter++;
+    //     if (color == 'random') {
+    //         color = setRandomColor();
+    //     }
+    //     square.style.backgroundColor = color;
+    //     squares.forEach(hoveredSquare => hoveredSquare.addEventListener('mouseover', () => {
+    //         if (clickCounter % 2 == 0) {
+    //             hoveredSquare.style.backgroundColor = 'white';
+    //         } else {
+    //             hoveredSquare.style.backgroundColor = color;
+    //         }
+    //     }));
     
-    }))
+    // }))
 }
