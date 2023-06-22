8e9bbb16054048e31a9e25f165c52bd5c30e2cd3
elenaeslin <srta.scobar@gmail.com>
2023-06-22 14:57:42 +0200

    Add first functions

---
+++ calculator/script.js
@@ -0,0 +1,22 @@
+
+
+function add(a, b) {
+    return a + b;
+}
+function substract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a  / b;
+}
+
+let a = 2;
+let b = 2;
+
+console.log(add(a, b));
+console.log(substract(a, b));
+console.log(multiply(a, b));
+console.log(divide(a, b));
