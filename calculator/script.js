4f45e031dc840016e2e130cd99a3b3378e61caf9
elenaeslin <srta.scobar@gmail.com>
2023-06-28 11:35:58 +0200

    Restart functions

--- calculator/script.js
+++ calculator/script.js
@@ -1,32 +1,50 @@
-const btns = document.querySelectorAll('button');
+const numBtns = document.querySelectorAll('.num-btn');
 const result = document.getElementById('result');
 const regexNum = /[123456789]/;
 
 let op1 = '', op2 = '', operator = '';
 let counter = 0;
+let res = 0;
 
-btns.forEach(btn => btn.addEventListener('click', () => {
-    if (regexNum.test(btn.textContent)) {
-        if (counter % 2 == 0) {
-            op1 += btn.textContent;
-            console.log(op1)
-        } else {
-            op2 += btn.textContent;
-        }
-        result.textContent += btn.textContent
-    } else if (btn.textContent == '=') {
-        op1 = Number(op1);
-        op2 = Number(op2);
-        let res = operate(op1, operator, op2);
-        // console.log(res);
-        result.textContent = res;
-        
-    } else {
-        counter++;
-        operator = btn.textContent;
-        result.textContent += btn.textContent;
-    }
-}));
+function main() {
+    numBtns.forEach(btn => btn.addEventListener('click', () => {
+        result.textContent = btn.textContent;
+    }));
+}
+
+// btns.forEach(btn => btn.addEventListener('click', () => {
+//     if (regexNum.test(btn.textContent)) {
+//         if (res != 0) {
+//             op1 = '';
+//             op2 = '';
+//         }
+//         if (counter % 2 == 0) {
+//             op1 += btn.textContent;
+//             result.textContent = op1;
+//         } else {
+//             op2 += btn.textContent;
+//             result.textContent = op2;
+//         }
+//     } else {
+//         if (op1 != '' && op2 != '') {
+//             op1 = Number(op1);
+//             op2 = Number(op2);
+//             res += operate(op1, operator, op2);
+//             result.textContent = res;
+//         }
+
+
+//         if (btn.textContent == '=') {
+
+//         } else {
+//             operator = btn.textContent;
+//         }
+
+
+//         counter++;
+
+//     } 
+// }));
 
 function add(a, b) {
     return a + b;
@@ -58,3 +76,5 @@ let b = 2;
 let op = '/'
 
 console.log(operate(a, op, b));
+
+main();
