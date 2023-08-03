90d0a8d9732a5a355344d1cdcace5badad9f898c
elenaeslin <srta.scobar@gmail.com>
2023-08-03 18:25:37 +0200

    Add form

--- sign-up-form/script.js
+++ sign-up-form/script.js
@@ -2,6 +2,8 @@ const openBtns = document.querySelectorAll('.open__action');
 const closeBtn = document.getElementById('closeBtn');
 const modal = document.getElementById('modal');
 const header = document.getElementById('header');
+const input = document.querySelector("#phone");
+const inputs = document.querySelectorAll('input');
 
 
 openBtns.forEach(openBtn => openBtn.addEventListener('click', () => {
@@ -12,5 +14,28 @@ openBtns.forEach(openBtn => openBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
     modal.classList.remove('open');
     header.classList.remove('modal');
-})
+});
 
+
+
+window.intlTelInput(input, {
+    initialCountry: "auto",
+    geoIpLookup: callback => {
+        fetch("https://ipapi.co/json")
+          .then(res => res.json())
+          .then(data => callback(data.country_code))
+          .catch(() => callback("us"));
+      },
+    utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/utils.js",
+});
+
+inputs.forEach(input => {
+    input.addEventListener('input', () => {
+        console.log(input.value.trim())
+        if(input.value.trim() !== '') {
+            input.classList.add('filled');
+        } else {
+            input.classList.remove('filled');
+        }
+    })
+})
