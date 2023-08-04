28790b2a2685cb713c06b4d49e8c31a8cc0eae29
elenaeslin <srta.scobar@gmail.com>
2023-08-04 09:24:51 +0200

    Complete form and functionalities

--- sign-up-form/script.js
+++ sign-up-form/script.js
@@ -5,6 +5,17 @@ const header = document.getElementById('header');
 const input = document.querySelector("#phone");
 const inputs = document.querySelectorAll('input');
 
+// input control
+const fileContainer = document.querySelector('.file-input-container');
+const fileInput = document.getElementById('cv');
+const fileLabel = document.getElementById('file-label');
+const fileText = document.getElementById('file-text');
+const progressBar = document.getElementById('progress-bar');
+const progressBarAfter = document.getElementById('progress-bar--after');
+const deleteIcon = document.getElementById('delete-icon');
+
+const initialFileText = fileText.innerHTML;
+
 
 openBtns.forEach(openBtn => openBtn.addEventListener('click', () => {
     modal.classList.add('open');
@@ -38,4 +49,71 @@ inputs.forEach(input => {
             input.classList.remove('filled');
         }
     })
+});
+
+//input control
+fileLabel.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    fileLabel.style.borderStyle = 'solid';
+});
+fileLabel.addEventListener('dragleave', (e) => {
+    e.preventDefault();
+    fileLabel.style.borderStyle = 'dashed';
+});
+fileLabel.addEventListener('drop', (e) => {
+    e.preventDefault();
+    const file = e.dataTransfer.files[0];
+    uploadFile(file);
+    
+});
+fileInput.addEventListener('change', () => {
+    const file = fileInput.files[0];
+    uploadFile(file);
 })
+
+deleteIcon.addEventListener('click', () => {
+    resetFileInput();
+    fileContainer.appendChild(fileInput);
+})
+
+function uploadFile(file) {
+    const totalSize = file.size;
+    let uploadedSize = 0;
+
+    const updateProgressBar = () => {
+        let progress = (uploadedSize / totalSize) * 100;
+        progressBarAfter.style.width = `${progress}%`;
+
+        if (progress >= 100) {
+            fileText.style.display = 'block';
+            fileText.textContent = file.name;
+            fileText.style.textAlign = 'left';
+            fileContainer.removeChild(fileInput);
+            progressBar.style.display = 'none';
+        }
+    };
+    const simulateUpload = () => {
+        fileText.style.display = 'none';
+        fileLabel.style.border = 'none';
+        setTimeout(() => {
+            uploadedSize += 10000;
+            updateProgressBar();
+    
+            if (uploadedSize < totalSize) {
+                simulateUpload();
+            }
+    
+        }, 50);
+    }
+    progressBarAfter.style.width = "0%";
+    progressBar.style.display = 'block';
+    deleteIcon.style.display = 'block';
+    simulateUpload();
+}
+
+function resetFileInput() {
+    deleteIcon.style.display = 'none';
+    fileText.innerHTML = initialFileText;
+    fileText.style.textAlign = 'center';
+    fileLabel.style.border = '1px dashed rgba(255,255,255,.5)';
+}
